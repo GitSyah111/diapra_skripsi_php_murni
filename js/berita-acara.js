f6f208fea540e18e8e4c67075c2bb1861e8f7c10
GitSyah111 <syahibnu704@gmail.com>
2026-07-10 18:16:21 +0800

    Revert "fix berita acara"
    This reverts commit 556b199a2b26b625d9aae615f3d77dc2ca46db05.

--- js/berita-acara.js
+++ js/berita-acara.js
@@ -50,22 +50,22 @@ $(document).ready(function() {
         pageLength: 10,
         columnDefs: [
             {
-                targets: 0,
+                targets: 0, // Column #
                 orderable: false,
-                searchable: false,
-                className: 'text-center'
+                className: 'select-checkbox'
             }
         ],
+        select: {
+            style: 'multi',
+            selector: 'td:first-child'
+        },
         order: [[ 1, 'asc' ]]
     });
 
-    // Menambahkan nomor urut otomatis
-    table.on('order.dt search.dt', function () {
-        let i = 1;
-        table.cells(null, 0, { search: 'applied', order: 'applied' }).every(function (cell) {
-            this.data(i++);
-        });
-    }).draw();
+    // Menangani klik baris untuk seleksi (optional tapi style di gambar ada kolom #)
+    $('#beritaAcaraTable tbody').on('click', 'tr td:first-child', function() {
+        $(this).parent().toggleClass('selected');
+    });
 
 });
 
